ce00b1100630b95be60816d107ec517d26cc8c1a
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-12-30 13:39:27 +0200

    List Persistence

--- Altce/List/List.tsx
+++ Altce/List/List.tsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 import ListItems from './ListItems'
 import NewListItemField from './NewListItemField'
 
 const List = () => {
   const [ listItems, setListItems ] = useState<string[]>([])
+  const { getItem, setItem } = useAsyncStorage('listItems')
+
+  const saveListItems = () => {
+    const json = JSON.stringify(listItems)
+    setItem(json)
+  }
+
+  const loadListItems = () => {
+    getItem()
+      .then(json => JSON.parse(json || ''))
+      .then(result => setListItems(result))
+  }
+
+  useEffect(() => loadListItems(), [])
+  useEffect(() => saveListItems(), [ listItems ])
+
+  const addListItem = (newListItem: string) => {
+    setListItems(listItems.concat(newListItem))
+  }
 
   return (
     <View style = { styles.main }>
       <ListItems listItems = { listItems }/>
-      <NewListItemField addListItem = { (newListItem: string) => setListItems([ ...listItems, newListItem ]) }/>
+      <NewListItemField addListItem = { addListItem }/>
     </View>
   )
 }
